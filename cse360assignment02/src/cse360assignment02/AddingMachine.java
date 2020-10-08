c30ff19fa0ac545a7da44999000d7429fc1cb9d4
Jaime Casas <Jaime Casas@DESKTOP-PPJHA8J>
2020-10-07 23:40:24 -0700

    Added new code for adding and subtracting.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,28 +1,52 @@
 package cse360assignment02;
 
+
 public class AddingMachine {
-  private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
+	public static void main(String[] args) {
+	    new AddingMachine().run();
+	} 
 
-  public void subtract (int value) {
-  }
 
-  public String toString () {
-    return "";
-  }
 
-  public void clear() {
-  }
+
+
+private void run() {
+	AddingMachine myCalculator = new AddingMachine();
+	myCalculator.add (4); 
+	myCalculator.subtract (2); 
+	myCalculator.add(5);
+	System.out.println(myCalculator);
+	}
+
+
+
+
+
+private int total;
+StringBuffer string;
   
-  
+public AddingMachine () {
+total = 0; // not needed - included for clarity
+string = new StringBuffer("0");
 }
+  
+public int getTotal () {
+return total;
+}
+  
+public void add (int value) {
+total = total +value;
+string.append(" + "+value);
+}
+  
+public void subtract (int value) {
+total = total -value;
+string.append(" - "+value);
+}
+  
+
+public String toString () {
+return string.toString();
+}
+}
+
